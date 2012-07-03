4b939db08d2eb35fda4ebef2a67862627dc37bd8
Lance Hepler <nlhepler@gmail.com>
2012-07-03 14:09:41 -0700

    fix vim-pathogen installation

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@ git submodule init
 git submodule update
 # ln -sf ~/.dot_files/.bashrc ~/.bashrc
 ln -sf ~/.dot_files/.profile ~/.profile
-mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/{undo,autoload}
 ln -sf ~/.dot_files/.vimrc ~/.vimrc
 ln -sf ~/.dot_files/.vim ~/.vim
 ln -sf ~/.dot_files/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dot_files/.zshrc ~/.zshrc
+ln -sf ~/.dot_files/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
