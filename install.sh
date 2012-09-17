bea849b45f687d98575e972ba9beb5c7a462b0fa
Lance Hepler <nlhepler@gmail.com>
2012-09-17 14:09:11 -0700

    fix install script, remove pointless .vimrc from .vim directory

--- install.sh
+++ install.sh
@@ -2,11 +2,21 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 git submodule init
 git submodule update
-# ln -sf ${DIR}/.bashrc ~/.bashrc
-ln -sf ${DIR}/.profile ~/.profile
+
+# link in the vim environment 
+if [ -L ~/.vim ]; then
+    rm ~/.vim
+else if [ -d ~/.vim ]; then
+    echo "~/.vim directory exists! aborting!"
+fi
+fi
 ln -sf ${DIR}/.vim ~/.vim
+
 mkdir -p ~/.vim/{undo,autoload}
 mkdir -p ~/.bin
+
+# ln -sf ${DIR}/.bashrc ~/.bashrc
+ln -sf ${DIR}/.profile ~/.profile
 ln -sf ${DIR}/.bin/* ~/.bin
 ln -sf ${DIR}/.gitconfig ~/.gitconfig
 ln -sf ${DIR}/.npmrc ~/.npmrc
